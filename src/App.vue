dc4e4e4e1b5f2778c7769962e812a6a817c0bda9
Carl Hain <crhain@gmail.com>
2019-11-10 10:16:14 -0600

    Import Youtube api key into App component from hidden credentials file

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
-    <SearchBar></SearchBar>
+    <SearchBar @termChange="onTermChange"></SearchBar>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
+import YouTube from '../credentials/youtube.js';
 import SearchBar from './components/SearchBar';
 
 //Note: need to list all components used in components property
@@ -12,6 +14,11 @@ export default {
   name: 'App',
   components: {
     SearchBar
+  },
+  methods: {
+    onTermChange(searchTerm){
+      console.log(searchTerm);
+    }
   }  
 };
 </script>
