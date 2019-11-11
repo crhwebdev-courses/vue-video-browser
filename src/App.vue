bba6a93e2c8c03fc94ca2f92149f7b46e015f3cf
Carl Hain <crhain@gmail.com>
2019-11-11 15:03:04 -0600

    Add some layout styling so that VideoList and VideoDetail are in the correct location

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="container">
     <SearchBar @termChange="onTermChange"></SearchBar>
-    <VideoDetail :video="selectedVideo"></VideoDetail>
-    <VideoList :videos="videos" @videoSelect="onVideoSelect"></VideoList>
+    <div class="row">
+      <VideoDetail :video="selectedVideo"></VideoDetail>
+      <VideoList :videos="videos" @videoSelect="onVideoSelect"></VideoList>
+    </div>
+    
   </div>
 </template>
 
